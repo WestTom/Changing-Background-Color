39e05af17f414aeebac846e285d77b65fdac0441
Coding Ba Kelvin <83387330+codingbagh@users.noreply.github.com>
2022-06-28 00:33:10 +0100

    .

--- changeColor.js
+++ changeColor.js
@@ -1,16 +1,23 @@
-const colorInput=document.querySelector("#colorInput");
-const colorBtn=document.querySelector("#colorButton");
-const body=document.querySelector("#body");
+const colorBtn = document.querySelector("#colorButton");
+const body = document.querySelector("#body");
 
 //function to change the background color
 
-const changeColor = () =>{
-    const colorInput=document.querySelector("#colorInputColor");
-    const colorBtn=document.querySelector("#colorButton");
-    const body=document.querySelector("#body");
+const changeColor = () => {
+  const colorInput = document.querySelector("#colorInputColor");
+  const colorBtn = document.querySelector("#colorButton");
+  const body = document.querySelector("#body");
 
-    let color = colorInput.Value;
-    body.style.backgroundColor=color;
-}; 
+  //the bug=> value starts with small letter [ previous code =>  let color = colorInput.Value;]
+  let color = colorInput.value;
+  body.style.backgroundColor = color;
 
-colorBtn.addEventListener("click",changeColor);
+  //I added this code to make the text input work with the color input
+  const colorInputText = document.querySelector("#colorInput");
+  colorInputText.value != ""
+    ? ((body.style.backgroundColor = colorInputText.value),
+      (colorInputText.value = ""))
+    : (body.style.backgroundColor = color);
+};
+
+colorBtn.addEventListener("click", changeColor);
