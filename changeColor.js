86549fcf091a0b765111c3f9f25ec33ed2dcb4de
CaptainWest <100836280+WestTom@users.noreply.github.com>
2022-06-27 05:58:09 -1200

    Add files via upload

---
+++ changeColor.js
@@ -0,0 +1,16 @@
+const colorInput=document.querySelector("#colorInput");
+const colorBtn=document.querySelector("#colorButton");
+const body=document.querySelector("#body");
+
+//function to change the background color
+
+const changeColor = () =>{
+    const colorInput=document.querySelector("#colorInputColor");
+    const colorBtn=document.querySelector("#colorButton");
+    const body=document.querySelector("#body");
+
+    let color = colorInput.Value;
+    body.style.backgroundColor=color;
+}; 
+
+colorBtn.addEventListener("click",changeColor);
